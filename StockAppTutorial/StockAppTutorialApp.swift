749e2cc34c2495ae61bc2437c613303cc3770def
Joao Gripp <corguinha@gmail.com>
2022-12-09 11:28:34 +0000

    Add SPM StockAPI

--- StockAppTutorial/StockAppTutorialApp.swift
+++ StockAppTutorial/StockAppTutorialApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import StocksAPI
 
 @main
 struct StockAppTutorialApp: App {
+    
+    let stocksAPI = StocksAPI()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear() {
+                    Task {
+                        do {
+                            let quotes = try await stocksAPI.fetchQuotes(symbols: "AAPL")
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
         }
     }
 }
