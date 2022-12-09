0a8b11905945d42abf32531278b1eb8f55e16ea2
Joao Gripp <corguinha@gmail.com>
2022-12-09 11:20:33 +0000

    Initial Commit

---
+++ StockAppTutorial/StockAppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppTutorialApp.swift
+//  StockAppTutorial
+//
+//  Created by Joao Gripp on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StockAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
